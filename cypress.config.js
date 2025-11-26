435c716d9d24c92952cde614f7294c4fee2e91e4
Pablo Alexandre dos Reis <pablo.reis@southsystem.com.br>
2025-11-26 15:20:20 -0300

    estudo cypress prompt

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,10 @@
-module.exports = {
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
   projectId: "xr8k2i",
   e2e: {
     setupNodeEvents(on, config) {
-      // ...
+      // registre eventos aqui se precisar
     },
-  },
-}
-
+  },  
+});
