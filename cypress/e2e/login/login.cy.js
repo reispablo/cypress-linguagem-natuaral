0658e07b28005963521cc6b32dc3205bb0023fa4
Pablo Alexandre dos Reis <pablo.reis@southsystem.com.br>
2025-11-27 09:43:31 -0300

    estudo cypress prompt

--- cypress/e2e/login/login.cy.js
+++ cypress/e2e/login/login.cy.js
@@ -3,25 +3,25 @@ describe('Exemplo de uso do cy.prompt', () => {
         describe('Login - Plataforma Quanta', () => {
             it('should login using CPF and password', () => {
 
-                cy.prompt(
-                    [
-                        'visit https://plataforma.hlg.portalquanta.com.br/login',
-                        'type "04598956054" into the CPF field',
-                        'type "Test@mobi25" into the password field',
-                        'click the "Entrar na plataforma" button'
-                    ],
-                    {
-                        // opcional – ajuda a IA a escolher seletores mais fortes
-                        includeGenerativeStepsLog: true,
-                        selectorHints: [
-                            'data-testid',
-                            'placeholder',
-                            'label',
-                            'text',
-                            'value'
-                        ]
-                    }
-                )
+cy.prompt(
+                    [
+                        'visit https://plataforma.hlg.portalquanta.com.br/login',
+                        'type "04598956054" into the CPF field',
+                        'type "Test@mobi25" into the password field',
+                        'click the "Entrar na plataforma" button'
+                    ],
+                    {
+                        // opcional – ajuda a IA a escolher seletores mais fortes
+                        includeGenerativeStepsLog: true,
+                        selectorHints: [
+                            'data-testid',
+                            'placeholder',
+                            'label',
+                            'text',
+                            'value'
+                        ]
+                    }
+                )
             })
         })
     })
